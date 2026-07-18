711bd0ad7f8c9a66ae478c0cad7bdfac22184e0b
victormetral <victor.metral@gmail.com>
2026-07-18 16:13:15 +0200

    Corrige les icônes Leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,6 +3,16 @@ import { useEffect, useRef } from "react"
 import L from "leaflet"
 import "leaflet/dist/leaflet.css"
 
+import markerIcon from "leaflet/dist/images/marker-icon.png"
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
+import markerShadow from "leaflet/dist/images/marker-shadow.png"
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+})
+
 const creerContenuPopup = (lieu) => {
   const conteneur = document.createElement("div")
 
